b10e5bf4058e2ae2c7bcdb2fe4666d5e2f040ea5
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-08-30 12:25:47 +0530

    update

--- app/Console/Commands/ScrapeLinks.php
+++ app/Console/Commands/ScrapeLinks.php
@@ -10,7 +10,7 @@ use Symfony\Component\DomCrawler\Crawler;
 
 class ScrapeLinks extends Command
 {
-    protected $signature = 'scraped:links {id} {url} {page}';
+    protected $signature = 'scrape:links {id} {url} {page}';
     protected $description = 'Scrape all links from a website';
 
     public function __construct()
@@ -28,7 +28,51 @@ class ScrapeLinks extends Command
         $queue = [$startingUrl];
         $visited = [];
 
-       /*  */
+        while (!empty($queue)) {
+            $url = array_shift($queue);
+
+            if (!in_array($url, $visited)) {
+                try {
+                    $response = Http::get($url);
+                    $crawler = new Crawler($response->body(), $url); // Pass the base URL to the constructor
+
+                    $links = $crawler->filter('a')->links();
+
+                    foreach ($links as $link) {
+                        $href = $link->getUri();
+                        $parsedUrl = parse_url($href);
+                        $imageExtensions = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'svg']; // Add more if needed 
+
+                        if (isset($parsedUrl['host']) && $parsedUrl['host'] === $domain && isset($parsedUrl['path'])) {
+                            if (!isset($parsedUrl['scheme'])) {
+                                // Handle relative URLs by constructing an absolute URL
+                                $href = $url . (isset($parsedUrl['path']) ? $parsedUrl['path'] : '') . (isset($parsedUrl['query']) ? '?' . $parsedUrl['query'] : '');
+                            }
+
+                            // Check if the URL extension is in the imageExtensions array
+                            $urlExtension = pathinfo($parsedUrl['path'], PATHINFO_EXTENSION);
+                            if (!in_array(strtolower($urlExtension), $imageExtensions) && !Str::contains($parsedUrl['path'], '#')) {
+                                $queue[] = $href;
+                            }
+                        }
+                    }
+
+                    $visited[] = $url;
+
+                    if (strpos($url, $productsLink) !== false) {
+                        $this->storeLinks($url, $id, $response->body()); // Store the link in the database
+
+                        $this->info("Links found on $startingUrl: $url");
+                    } else {
+                        $this->info("Links not found on $startingUrl : $url");
+                    }
+                } catch (\Throwable $th) {
+                    //throw $th;
+                }
+            }
+            sleep(0.5);
+        }
+
         $this->info('Scraping completed.');
     }
 
